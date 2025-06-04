eb6f20391cf6b95875e6750a625fbdbd73c1da86
A.Kerem Erdoğmuş <akerdogmus@gmail.com>
2025-06-04 11:18:27 +0300

    Refactor web link functions

--- extras.py
+++ extras.py
@@ -94,13 +94,13 @@ def str_splitter(str_to_split):
 def open_website(url):
     webbrowser.open(url)
 
-def camfitool_web(self):
+def camfitool_web():
     url = "https://github.com/inomuh/camfitool"
     open_website(url)
     # self.pop_up_info_message('This section will be added!')
-def valu3s_web(self):
+def valu3s_web():
     url="https://valu3s.eu"
     open_website(url)
-def imtgd_web(self):
+def imtgd_web():
     url="https://www.inovasyonmuhendislik.com/"
     open_website(url)
